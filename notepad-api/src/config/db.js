a7c11dc18240256448f1e0ab8c627ec335bc559e
xavierjosefs <xavierfernandezsantana.com>
2025-10-28 13:59:06 -0400

    fix: conexion a supabase

--- notepad-api/src/config/db.js
+++ notepad-api/src/config/db.js
@@ -1,5 +1,8 @@
 import pg from "pg";
+import dns from "dns";
 import "dotenv/config";
+
+dns.setDefaultResultOrder("ipv4first");
 
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
